c270aec95c5c4fd65f7cff66c362db33a0017f70
heinrichAdams <hein.peter.adams@gmail.com>
2023-04-12 15:59:55 +0100

    Display and font settings added

--- Source/main.py
+++ Source/main.py
@@ -1,3 +1,32 @@
-import pygame
+import pygame,sys
+from settings import *
 
-print("Working")
+# <<<<<<<<<<<<<<<<<< GAME CLASS
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_X,SCREEN_Y))
+        pygame.display.set_caption("KosmichE")
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            delta_time = self.clock.tick() / 1000
+            pygame.display.update()
+
+# >>>>>>>>>>>>>>>>>> GAME CLASS
+
+
+# <<<<<<<<<<<<<<<<<< MAIN
+
+if __name__ == "__main__":
+    current_game = Game()
+    current_game.run()
+
+# >>>>>>>>>>>>>>>>>> MAIN
---
+++ Source/settings.py
@@ -0,0 +1,10 @@
+
+# Various Project Settings
+
+# <<<<<<<<<<<<<<<<<<
+# >>>>>>>>>>>>>>>>>>
+
+# Screen
+SCREEN_X = 1280
+SCREEN_Y = 720
+TILE_SIZE = 64
