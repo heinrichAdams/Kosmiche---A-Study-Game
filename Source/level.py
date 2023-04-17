535b114ebc6b5357a93207b0873b43d52e1bde73
heinrichAdams <hein.peter.adams@gmail.com>
2023-04-17 13:49:43 +0100

    Refactoring

--- Source/level.py
+++ Source/level.py
@@ -9,7 +9,7 @@ class Level:
 
     def __init__(self):
         self.main_display_surface = pygame.display.get_surface()
-        self.master_sprite_group = pygame.sprite.Group()
+        self.master_sprite_group = CameraGroup()
         self.setup()
         self.hud = Hud(self.current_player)
     def setup(self):
@@ -17,8 +17,25 @@ class Level:
 
     def run(self, delta_time):
         self.main_display_surface.fill("White")
-        self.master_sprite_group.draw(self.main_display_surface)
+        self.master_sprite_group.draw()
         self.master_sprite_group.update(delta_time)
         self.hud.display_inventory()
+
+
+# >>>>>>>>>>>>>>>>>> LEVEL CLASS
+
+
+# <<<<<<<<<<<<<<<<<< LEVEL CLASS
+
+class CameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.main_display_surface = pygame.display.get_surface()
+
+    def draw(self):
+        for sprite in self.sprites():
+            self.main_display_surface.blit(sprite.image, sprite.rect)
+
+
 
 # >>>>>>>>>>>>>>>>>> LEVEL CLASS
---
+++ Source/sprites.py
@@ -0,0 +1,12 @@
+import pygame
+from settings import *
+
+# <<<<<<<<<<<<<<<<<< BASE CLASS
+
+class Base(pygame.sprite.Sprite):
+    def __init__(self, position, image, group):
+        super().__init__(group)
+        self.image
+        self.rec
+
+# >>>>>>>>>>>>>>>>>> BASE CLASS
