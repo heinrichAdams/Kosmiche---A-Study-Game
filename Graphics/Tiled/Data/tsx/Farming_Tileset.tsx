5be870702326a12c27ea0d27e7028774621b0864
s1409 <80841726+IrmaAdams@users.noreply.github.com>
2023-04-19 09:29:51 +0100

    Update folders
    deleted files not in use
    updated folder names

---
+++ Graphics/Tiled/Data/tsx/Farming_Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Farming_Tileset" tilewidth="64" tileheight="64" tilecount="308" columns="28">
+ <image source="../../../Environment/Farming/Asprite files/Farming_Tileset.png" width="1808" height="718"/>
+</tileset>
