3f645e3d256d33ad9ca85081c1fb947305777851
s1409 <80841726+IrmaAdams@users.noreply.github.com>
2023-05-10 13:16:11 +0100

    Tiled update
    Layers / Maps completed

---
+++ Graphics/Tiled/NewTiled/Buildings.tsx
@@ -0,0 +1,94 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Buildings" tilewidth="1280" tileheight="720" tilecount="30" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="256" height="256" source="../../Environment/Farming/Fences/Fences4.png"/>
+ </tile>
+ <tile id="1">
+  <image width="256" height="256" source="../../Environment/Farming/Fences/Fences5.png"/>
+ </tile>
+ <tile id="2">
+  <image width="256" height="256" source="../../Environment/Farming/Fences/Fences1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="256" height="256" source="../../Environment/Farming/Fences/Fences2.png"/>
+ </tile>
+ <tile id="4">
+  <image width="256" height="256" source="../../Environment/Farming/Fences/Fences3.png"/>
+ </tile>
+ <tile id="5">
+  <image width="71" height="76" source="../../Environment/House/House/table.png"/>
+ </tile>
+ <tile id="6">
+  <image width="54" height="92" source="../../Environment/House/House/bed.png"/>
+ </tile>
+ <tile id="7">
+  <image width="18" height="40" source="../../Environment/House/House/bedside table.png"/>
+ </tile>
+ <tile id="8">
+  <image width="116" height="89" source="../../Environment/House/House/book shelves.png"/>
+ </tile>
+ <tile id="9">
+  <image width="97" height="66" source="../../Environment/House/House/carpet1.png"/>
+ </tile>
+ <tile id="10">
+  <image width="97" height="66" source="../../Environment/House/House/carpet2.png"/>
+ </tile>
+ <tile id="11">
+  <image width="22" height="43" source="../../Environment/House/House/chairs1.png"/>
+ </tile>
+ <tile id="12">
+  <image width="22" height="43" source="../../Environment/House/House/chairs2.png"/>
+ </tile>
+ <tile id="13">
+  <image width="22" height="43" source="../../Environment/House/House/chairs3.png"/>
+ </tile>
+ <tile id="14">
+  <image width="22" height="43" source="../../Environment/House/House/chairs4.png"/>
+ </tile>
+ <tile id="15">
+  <image width="54" height="103" source="../../Environment/House/House/fireplace.png"/>
+ </tile>
+ <tile id="16">
+  <image width="441" height="661" source="../../Environment/House/House/House.png"/>
+ </tile>
+ <tile id="17">
+  <image width="1280" height="720" source="../../Environment/House/House/House_Interior.png"/>
+ </tile>
+ <tile id="18">
+  <image width="97" height="48" source="../../Environment/Shop/Shop objects/Shop_vegetable rack.png"/>
+ </tile>
+ <tile id="19">
+  <image width="615" height="671" source="../../Environment/Shop/Shop objects/Shop.png"/>
+ </tile>
+ <tile id="20">
+  <image width="83" height="47" source="../../Environment/Shop/Shop objects/Shop_counter back.png"/>
+ </tile>
+ <tile id="21">
+  <image width="119" height="64" source="../../Environment/Shop/Shop objects/Shop_counter front.png"/>
+ </tile>
+ <tile id="22">
+  <image width="121" height="105" source="../../Environment/Shop/Shop objects/Shop_counter.png"/>
+ </tile>
+ <tile id="23">
+  <image width="48" height="125" source="../../Environment/Shop/Shop objects/Shop_display shelves1.png"/>
+ </tile>
+ <tile id="24">
+  <image width="48" height="125" source="../../Environment/Shop/Shop objects/Shop_display shelves2.png"/>
+ </tile>
+ <tile id="25">
+  <image width="48" height="125" source="../../Environment/Shop/Shop objects/Shop_display shelves3.png"/>
+ </tile>
+ <tile id="26">
+  <image width="19" height="38" source="../../Environment/Shop/Shop objects/Shop_flowers.png"/>
+ </tile>
+ <tile id="27">
+  <image width="86" height="54" source="../../Environment/Shop/Shop objects/Shop_Notice Board.png"/>
+ </tile>
+ <tile id="28">
+  <image width="353" height="206" source="../../Environment/Shop/Shop objects/Shop_Objects.png"/>
+ </tile>
+ <tile id="29">
+  <image width="969" height="595" source="../../Environment/Shop/Shop objects/Shop_Tileset.png"/>
+ </tile>
+</tileset>
---
+++ Graphics/Tiled/NewTiled/Roofs.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Roofs" tilewidth="615" tileheight="671" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="615" height="671" source="../../Environment/Shop/Shop objects/Shop.png"/>
+ </tile>
+ <tile id="1">
+  <image width="441" height="661" source="../../Environment/House/House/House.png"/>
+ </tile>
+</tileset>
